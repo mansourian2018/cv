f684b72e56655c59174429f6b5aface5ddd061eb
unknown <mansourina2018@gmail.com>
2020-10-28 19:38:12 +0330

    change index cv font family

--- resources/views/cv.blade.php
+++ resources/views/cv.blade.php
@@ -4,6 +4,10 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="اصغر منصوریان">
+    <meta name="description" content="رزومه اصغر منصوریان">
+    <meta name="description" content="رزومه">
+    <meta name="description" content="رزومه اصغر منصوریان">    
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <title>اصغر منصوریان</title>
     <!-- === Bootstrap=== -->
