8fe5261f21df68202719a997fbed5b8c988d903a
Asghar Mansourian <mansourian2018@gmil.com>
2020-12-11 18:43:58 +0330

    test

--- app/Http/Controllers/ContentController.php
+++ app/Http/Controllers/ContentController.php
@@ -24,8 +24,6 @@ class ContentController extends Controller
             'code'      =>  401,
             'message'   =>  $validator->messages()->get('*')
         ), 401);
-
-
         }
         $content = new Content();
         $content->name = $request['name'];
